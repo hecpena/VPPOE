0b219d2ed640688bebab66c139f55175730c018c
Victor Alvarez <vmac179@gmail.com>
2021-08-02 19:51:03 -0400

    fix post data

--- lo.php
+++ lo.php
@@ -1,32 +1,29 @@
-<?php require_once('api_mt_include2.php'); ?>
 <?php
 
- 
-$ipRouterOS="192.168.88.1";  // IP tu RouterOS. 
-$Username="secretppoe"; 
-$Pass="123456"; 
-$api_puerto=8728; 
+    require_once('api_mt_include2.php');
+
+    $ipRouterOS="192.168.88.1";  // IP tu RouterOS.
+    $Username="secretppoe";
+    $Pass="123456";
+    $api_puerto=8728;
 
 
-$name = "Rodrigo";  // ---- nombre del usuario pppoe
-$password = "qwerty";  // 
-$service = "pppoe";
-$profile = "GPON-500";
-
-
+    $name = $_POST['nombre'];
+    $password = $_POST['password'];
+    $service = 'PPPOE';
+    $profile = $_POST['plan'];
 
     $API = new routeros_api();
     $API->debug = false;
-	
+
 	if ($API->connect($ipRouterOS, $Username, $Pass, $api_puerto)) {
 
-   $API->comm("/ppp/secret/add", array(
-      "name"     => "$name",
-      "password" => "$password",
-      "service"  => "$service",
-	  "profile"  => "$profile",
-   ));
-   
+    $API->comm("/ppp/secret/add", array(
+            "name"     => "$name",
+            "password" => "$password",
+            "service"  => "$service",
+            "profile"  => "$profile",
+    ));
+
     }
 ?>
-
